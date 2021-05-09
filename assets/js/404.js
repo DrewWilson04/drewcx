46c9b7644192eff6b4328d287ad0d406ed5cba49
drew <drewwilson2022@gmail.com>
2021-05-08 22:18:08 -0400

    redid whole website

---
+++ assets/js/404.js
@@ -0,0 +1,33 @@
+GitHubCalendar(".calendar", "mr-winson", {
+    responsive: true
+});
+
+var pullChain = document.getElementById("pull-chain");
+var css = document.getElementById("css");
+var toggled = false
+
+pullChain.addEventListener("click", function () {
+    startTimer();
+}, false);
+
+function startTimer() {
+    pullChain.classList.toggle("pulled");
+    setTimeout(stopTimer, 500);
+}
+
+function stopTimer() {
+    toggleLights();
+    pullChain.classList.toggle("pulled");
+}
+
+function toggleLights() {
+    if (toggled == true) {
+        css.setAttribute('href', '/assets/css/index.css')
+        graph.setAttribute('href', '/assets/css/dark_graph.css')
+        toggled = false
+    } else {
+        css.setAttribute('href', '/assets/css/light.css')
+        graph.setAttribute('href', '/assets/css/light_graph.css')
+        toggled = true
+    }
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,208 +1,72 @@
-/*
-	Paradigm Shift by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+GitHubCalendar(".calendar", "drewwilson04", {
+    responsive: true
+});
 
-(function($) {
+const projects = [{
+    "title": "Rung Boards",
+    "url": "https://rungboards.com",
+    "image": "rung-boards.jpg",
+    "class": "branches"
+},{
+    "title": "Cryzza",
+    "url": "https://cryzza.com/",
+    "urlname": "Visit (Work in Progress)",
+    "image": "Cryzza.svg",
+    "class": "branches"
+}]
 
-	var	$window = $(window),
-		$body = $('body');
+projects.reverse()
 
-	// Breakpoints.
-		breakpoints({
-			default:   ['1681px',   null       ],
-			xlarge:    ['1281px',   '1680px'   ],
-			large:     ['981px',    '1280px'   ],
-			medium:    ['737px',    '980px'    ],
-			small:     ['481px',    '736px'    ],
-			xsmall:    ['361px',    '480px'    ],
-			xxsmall:   [null,       '360px'    ]
-		});
+var projectsHtml = ``
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+for (i = 0; i < projects.length; i++) {
+    projectsHtml = projectsHtml + `
+            <div class="col-sm-6 col-xs-12 card-outer">
+                <a href="${projects[i].url}" class="link" target="_blank">
+                    <div class="card">
+                        <div class="card-image"><img src="/assets/img/${projects[i].image}"></div>
+                        <div class="card-title">${projects[i].title}</div>
+                    </div>
+                </a>
+            </div>`
+}
 
-	// Hack: Enable IE workarounds.
-		if (browser.name == 'ie')
-			$body.addClass('is-ie');
+document.getElementById('projects').innerHTML = projectsHtml
 
-	// Mobile?
-		if (browser.mobile)
-			$body.addClass('is-mobile');
+document.getElementById("pgp_button").addEventListener('click', function () {
+    document.getElementById("pgp").style.height = "unset"
+    document.getElementById("pgp_button").style.display = "none"
+})
 
-	// Scrolly.
-		$('.scrolly')
-			.scrolly({
-				offset: 100
-			});
+document.getElementById('pageHeight').innerText = `this page is ${document.body.scrollHeight} pixels tall`
 
-	// Polyfill: Object fit.
-		if (!browser.canUse('object-fit')) {
+var pullChain = document.getElementById("pull-chain");
+var css = document.getElementById("css");
+var graph = document.getElementById("graph");
+var toggled = false
 
-			$('.image[data-position]').each(function() {
+pullChain.addEventListener("click", function () {
+    startTimer();
+}, false);
 
-				var $this = $(this),
-					$img = $this.children('img');
+function startTimer() {
+    pullChain.classList.toggle("pulled");
+    setTimeout(stopTimer, 500);
+}
 
-				// Apply img as background.
-					$this
-						.css('background-image', 'url("' + $img.attr('src') + '")')
-						.css('background-position', $this.data('position'))
-						.css('background-size', 'cover')
-						.css('background-repeat', 'no-repeat');
+function stopTimer() {
+    toggleLights();
+    pullChain.classList.toggle("pulled");
+}
 
-				// Hide img.
-					$img
-						.css('opacity', '0');
-
-			});
-
-			$('.gallery > a').each(function() {
-
-				var $this = $(this),
-					$img = $this.children('img');
-
-				// Apply img as background.
-					$this
-						.css('background-image', 'url("' + $img.attr('src') + '")')
-						.css('background-position', 'center')
-						.css('background-size', 'cover')
-						.css('background-repeat', 'no-repeat');
-
-				// Hide img.
-					$img
-						.css('opacity', '0');
-
-			});
-
-		}
-
-	// Gallery.
-		$('.gallery')
-			.on('click', 'a', function(event) {
-
-				var $a = $(this),
-					$gallery = $a.parents('.gallery'),
-					$modal = $gallery.children('.modal'),
-					$modalImg = $modal.find('img'),
-					href = $a.attr('href');
-
-				// Not an image? Bail.
-					if (!href.match(/\.(jpg|gif|png|mp4)$/))
-						return;
-
-				// Prevent default.
-					event.preventDefault();
-					event.stopPropagation();
-
-				// Locked? Bail.
-					if ($modal[0]._locked)
-						return;
-
-				// Lock.
-					$modal[0]._locked = true;
-
-				// Set src.
-					$modalImg.attr('src', href);
-
-				// Set visible.
-					$modal.addClass('visible');
-
-				// Focus.
-					$modal.focus();
-
-				// Delay.
-					setTimeout(function() {
-
-						// Unlock.
-							$modal[0]._locked = false;
-
-					}, 600);
-
-			})
-			.on('click', '.modal', function(event) {
-
-				var $modal = $(this),
-					$modalImg = $modal.find('img');
-
-				// Locked? Bail.
-					if ($modal[0]._locked)
-						return;
-
-				// Already hidden? Bail.
-					if (!$modal.hasClass('visible'))
-						return;
-
-				// Stop propagation.
-					event.stopPropagation();
-
-				// Lock.
-					$modal[0]._locked = true;
-
-				// Clear visible, loaded.
-					$modal
-						.removeClass('loaded')
-
-				// Delay.
-					setTimeout(function() {
-
-						$modal
-							.removeClass('visible')
-
-						setTimeout(function() {
-
-							// Clear src.
-								$modalImg.attr('src', '');
-
-							// Unlock.
-								$modal[0]._locked = false;
-
-							// Focus.
-								$body.focus();
-
-						}, 475);
-
-					}, 125);
-
-			})
-			.on('keypress', '.modal', function(event) {
-
-				var $modal = $(this);
-
-				// Escape? Hide modal.
-					if (event.keyCode == 27)
-						$modal.trigger('click');
-
-			})
-			.on('mouseup mousedown mousemove', '.modal', function(event) {
-
-				// Stop propagation.
-					event.stopPropagation();
-
-			})
-			.prepend('<div class="modal" tabIndex="-1"><div class="inner"><img src="" /></div></div>')
-				.find('img')
-					.on('load', function(event) {
-
-						var $modalImg = $(this),
-							$modal = $modalImg.parents('.modal');
-
-						setTimeout(function() {
-
-							// No longer visible? Bail.
-								if (!$modal.hasClass('visible'))
-									return;
-
-							// Set loaded.
-								$modal.addClass('loaded');
-
-						}, 275);
-
-					});
-
-})(jQuery);
+function toggleLights() {
+    if (toggled == true) {
+        css.setAttribute('href', '/assets/css/index.css')
+        graph.setAttribute('href', '/assets/css/dark_graph.css')
+        toggled = false
+    } else {
+        css.setAttribute('href', '/assets/css/light.css')
+        graph.setAttribute('href', '/assets/css/light_graph.css')
+        toggled = true
+    }
+}
